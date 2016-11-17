4e3e488753361fdaab73abf3466e429c5fa9a7d8
yangxq <xiaoyxq@yahoo.com>
2016-11-17 15:19:35 +0800

    no message

--- js-task8/js/signup.js
+++ js-task8/js/signup.js
@@ -1,17 +1,17 @@
 myApp.controller('signupCtrl',function($scope,$state,$stateParams,$http,type,level){
 	$scope. types = type;
 	$scope.levels=level;
-	$scope.stType=function(){		
-		$scope.te=this.x.value;		
-	}
-
-	$scope.stLevel=function(){
-		$scope.ll=this.x.value;		
-	}
-
+	//$scope.stType=function(){
+	//	$scope.te=this.x.value;
+	//}
+    //
+	//$scope.stLevel=function(){
+	//	$scope.ll=this.x.value;
+	//}
+	$scope.params={};
 	$('#selectDate').datepicker(); //时间选择器
-
 	$scope.signUp=function(){
+		console.log($scope.params.type);
 		//$scope.date=(new Date($scope.jointime)).getTime();
 		// 添加
 		$scope.jointime=$('#selectDate').val(); //获得日期
@@ -22,10 +22,12 @@ myApp.controller('signupCtrl',function($scope,$state,$stateParams,$http,type,lev
 			params:{
 				name:$scope.name,
 				qq:$scope.qq,
-				type:$scope.te,
+				//type:$scope.te,
+				type:$scope.params.type,
 				school:$scope.school,
 				talent:$scope.tt,
-				level:$scope.ll,
+				//level:$scope.ll,
+				level:$scope.params.level,
 				joinTime:$scope.date,
 				wish:$scope.wish
 			},
